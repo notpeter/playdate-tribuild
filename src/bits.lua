10ea1d384da7dd6011439c02b97840d4b6301455
Peter Tripp <petertripp@gmail.com>
2023-04-19 11:57:10 -0400

    Triangles and shit.

--- src/bits.lua
+++ src/bits.lua
@@ -57,9 +57,12 @@ function bits.init()
     local left = point.new(61, 239)
     local right = point.new(338, 239)
 
-    local top1 = lerp2d(left, right, 0.5)
-    local left1 = lerp2d(top, right, 0.5)
-    local right1 = lerp2d(top, left, 0.5)
+    -- local top1 = lerp2d(left, right, 0.5)
+    -- local left1 = lerp2d(top, right, 0.5)
+
+    local right1 = lerp2d(right, lerp2d(top, left, 0.5), 1.5)
+    local left1 = lerp2d(left, lerp2d(top, right, 0.5), 1.5)
+    local top1 = lerp2d(top, lerp2d(left, right, 0.5), 1.5)
 
     new_top = lerper2d(top, top1, 100)
     new_left = lerper2d(left, left1, 100)
@@ -74,16 +77,12 @@ function bits.update()
     -- local top = new_top()
     local left = new_left()
     local right = new_right()
-
+    -- local top = new_top()
 
     gfx.clear()
     for i = 1,9 do
-        draw_line(top.x, top.y, lerp(left.x, right.x, i/10), left.y)
-    end
-    for i = 1,9 do
+        draw_line(top.x, top.y, lerp(left.x, right.x, i/10), lerp(left.y, right.y, i/10))
         draw_line(right.x, right.y, lerp(left.x, top.x, i/10), lerp(left.y, top.y, i/10))
-    end
-    for i = 1,9 do
         draw_line(left.x, left.y, lerp(right.x, top.x, i/10), lerp(right.y, top.y, i/10))
     end
 
